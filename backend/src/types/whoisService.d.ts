ef121a17df7571701b0101c617ab3728c5c89043
ivanenkomaksym <ivanenkomaxim@yahoo.com>
2025-04-05 14:24:39 +0300

    Use static types.

---
+++ backend/src/types/whoisService.d.ts
@@ -0,0 +1,42 @@
+export interface DomainInformation {
+    domainName?: string;
+    registryDomainId?: string;
+    updatedDate?: string;
+    creationDate?: string;
+    expirationDate?: string;
+    domainStatus?: string;
+    nameServer?: string;
+    dnssec?: string;
+}
+
+export interface RegistrarInformation {
+    registrar?: string;
+    registrarWhoisServer?: string;
+    registrarUrl?: string;
+    registrarIanaId?: string;
+    abuseContactEmail?: string;
+    abuseContactPhone?: string;
+}
+
+export interface ContactInfo {
+    name?: string;
+    organization?: string;
+    street?: string;
+    city?: string;
+    stateProvince?: string;
+    postalCode?: string;
+    country?: string;
+    phone?: string;
+    phoneExt?: string;
+    fax?: string;
+    faxExt?: string;
+    email?: string;
+}
+
+export interface WhoisData {
+    domainInformation: DomainInformation;
+    registrarInformation: RegistrarInformation;
+    registrantContact: ContactInfo;
+    adminContact: ContactInfo;
+    techContact: ContactInfo;
+}
