dabc7d632c8abdcb3997dc0bcbe9519993731afc
ivanenkomaksym <ivanenkomaxim@yahoo.com>
2025-04-06 10:46:40 +0300

    Add domain regex check. Add route tests.

---
+++ backend/src/__tests__/services/whois.test.ts
@@ -0,0 +1,53 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import router from '../../routes/whois';
+import request from 'supertest';
+import { lookupDomain } from '../../services/whoisService';
+import { WhoisData } from '../../types/whoisService';
+
+const app = express();
+app.use(bodyParser.json());
+app.use(router);
+
+jest.mock('../../services/whoisService');
+
+afterEach(() => {
+    jest.clearAllMocks();
+});
+
+describe('api', () => {
+    it ('should return WHOIS data for a valid domain', async () => {
+        const invalidDomain = 'google.com';
+                
+        // Mock the data returned by whois.lookup
+        const expectedResult: WhoisData = {
+            domainInformation: {
+                domainName: 'GOOGLE.COM'
+            },
+            registrarInformation: {},
+            registrantContact: {},
+            adminContact: {},
+            techContact: {},
+            geolocationData: {}
+        };
+
+        // Mocking the implementation of whois.lookup to return mock data
+        (lookupDomain as jest.Mock).mockImplementation((domain) => {
+            return expectedResult;
+        });
+
+        const response = await request(app).get(`/whois/${invalidDomain}`);
+        expect(response.status).toBe(200);
+        const result: WhoisData = response.body;
+
+        expect(result.domainInformation.domainName).toBe('GOOGLE.COM');
+    });
+
+    it('should fail when invalid domain', async () => {
+        const invalidDomain = 'invalid_domain';
+
+        const response = await request(app).get(`/whois/${invalidDomain}`);
+        expect(response.status).toBe(400);
+        expect(response.body.error).toBe('Invalid domain format');
+    });
+});
--- backend/src/routes/whois.ts
+++ backend/src/routes/whois.ts
@@ -1,16 +1,21 @@
 import express, {Request, Response} from 'express';
 import { lookupDomain } from '../services/whoisService';
+import { domainRegex } from '../consts';
 
 const router = express.Router();
 
 router.get('/whois/:domain', async (req: Request, res: Response) => {
     try {
         const { domain } = req.params;
-        console.log('WHOIS domain:', domain);
+        if (domainRegex.test(domain) === false) {
+            res.status(400).json({ error: 'Invalid domain format' });
+            return;
+        }
+
         const data = await lookupDomain(domain);
         res.json(data);
-    } catch (error) {
-        res.status(500).json({ error: 'Error fetching WHOIS data' });
+    } catch (detailedError) {
+        res.status(400).json({ error: 'Error fetching WHOIS data. ', detailedError: detailedError });
     }
 });
 
