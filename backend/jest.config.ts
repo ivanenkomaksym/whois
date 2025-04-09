c3e85899bc9d3d5012ca9d8e435a9363ff66d4bc
ivanenkomaksym <ivanenkomaxim@yahoo.com>
2025-04-09 14:24:18 +0300

    Ignore running tests in dist folder.

--- backend/jest.config.ts
+++ backend/jest.config.ts
@@ -1,4 +1,8 @@
 export default {
     preset: 'ts-jest',
     testEnvironment: 'node',
+    testPathIgnorePatterns: [
+        '/node_modules/', // Usually good to keep ignoring node_modules
+        '<rootDir>/dist/' // Add this line to ignore the dist directory
+      ],
 };
