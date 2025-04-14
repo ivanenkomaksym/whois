9e7fc4b391e66dce910833abbd97509466ef6ad2
ivanenkomaksym <ivanenkomaxim@yahoo.com>
2025-04-14 09:53:08 +0300

    Add middleware to check for secret header for all requests.

--- backend/src/consts.ts
+++ backend/src/consts.ts
@@ -1,1 +1,2 @@
 export const domainRegex = /^(?!-)[A-Za-z0-9-]{1,63}(?<!-)(\.[A-Za-z]{2,})+$/; // Regex to validate domain names
+export const secretHeaderName = 'X-Secret-Header'; // Header name for secret key
