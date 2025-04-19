391b89e12d61630a51f1f04ed35e558c53220a37
ivanenkomaksym <ivanenkomaxim@yahoo.com>
2025-04-19 11:52:39 +0300

    Automatically copy the functions directory into dist.

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    // Static copy plugin configuration
+    viteStaticCopy({
+      targets: [
+        {
+          // Copy the 'functions' directory from the project root (frontend/functions)
+          src: 'functions',
+          // Place it directly into the output directory (dist/functions)
+          dest: '.' // Destination is relative to the 'dist' directory
+        }
+      ]
+    })],
 })
