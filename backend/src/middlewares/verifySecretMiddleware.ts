50239ec8dd3269c81c1362b1a9e413ee20552672
ivanenkomaksym <ivanenkomaxim@yahoo.com>
2025-04-19 11:40:53 +0300

    Add logging to secret verify middleware.

--- backend/src/middlewares/verifySecretMiddleware.ts
+++ backend/src/middlewares/verifySecretMiddleware.ts
@@ -3,7 +3,9 @@ import { secretHeaderName } from "../../../shared/types/consts";
 function verifySecret(req: {
     headers: any; method: any;
 }, res: any, next: () => void) {
+    console.log("Verifying secret header...");
     if (!req.headers[secretHeaderName]) {
+        console.log("No secret header found.");
         return res.status(401).json({ error: 'Unauthorized' });
     }
     next();
