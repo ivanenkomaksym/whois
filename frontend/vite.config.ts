1f2723911f8302dbec54a0028de3f641f8c02882
ivanenkomaksym <ivanenkomaxim@yahoo.com>
2025-04-19 12:10:44 +0300

    Revert "Automatically copy the functions directory into dist."
    This reverts commit 391b89e12d61630a51f1f04ed35e558c53220a37.

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,20 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    // Static copy plugin configuration
-    viteStaticCopy({
-      targets: [
-        {
-          // Copy the 'functions' directory from the project root (frontend/functions)
-          src: 'functions',
-          // Place it directly into the output directory (dist/functions)
-          dest: '.' // Destination is relative to the 'dist' directory
-        }
-      ]
-    })],
+  plugins: [react()],
 })
