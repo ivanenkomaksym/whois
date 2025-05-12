5a04dda10ccac1589dc58434d9a44a59417a20ba
ivanenkomaksym <ivanenkomaxim@yahoo.com>
2025-05-12 09:28:26 +0300

    Fix checking header key, convert it to lower case. Check auth header key value.

--- backend/src/middlewares/verifySecretMiddleware.ts
+++ backend/src/middlewares/verifySecretMiddleware.ts
@@ -4,8 +4,12 @@ function verifySecret(req: {
     headers: any; method: any;
 }, res: any, next: () => void) {
     console.log("Verifying secret header...");
-    if (!req.headers[secretHeaderName]) {
+    const secretHeader = req.headers[secretHeaderName.toLocaleLowerCase()];
+    if (!secretHeader) {
         console.log("No secret header found.");
+        return res.status(401).json({ error: 'Unauthorized' });
+    } else if (secretHeader !== process.env.AUTH_SECRET_KEY) {
+        console.log("Invalid secret header.");
         return res.status(401).json({ error: 'Unauthorized' });
     }
     next();
